46cf64edfacfa812718bf944e7a0addeb552986d
Luis <arenasvillamizar.luiscarlos@gmail.com>
2023-08-22 15:09:40 -0500

    numerales

--- test_fuctions.py
+++ test_fuctions.py
@@ -6,11 +6,11 @@ def test_add():
 	assert add("space","ship")=="spaceship"
 
 
-#def test_subtract():
-#	assert subtract(2,3)==-1
+def test_subtract():
+	assert subtract(2,3)==-1
 
-#def test_covert_fahr_to_c():
-#	assert f2c(32)==0
-#	assert f2c(122)== pytest.approx(50)
-# 	with pytest.raises (AssertionError)
-#		f2c(-600)
+def test_covert_fahr_to_c():
+	assert f2c(32)==0
+	assert f2c(122)== pytest.approx(50)
+ 	with pytest.raises (AssertionError)
+		f2c(-600)
