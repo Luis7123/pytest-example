b5d4fefe0558f6c8c1226e9030a2dd1b5ac8d0d4
Luis <arenasvillamizar.luiscarlos@gmail.com>
2023-08-22 14:45:07 -0500

    crear los .py

---
+++ functions.py
@@ -0,0 +1,11 @@
+def add(a,b):
+	return a+b
+
+def subtract(a,b):
+	return a+b
+
+def multiply (a,b):
+	return a*b
+
+def convert_fahr_to_c(fahrenheit):
+	return multiply(subtract(fahrenheit,32),9/5)
---
+++ test_fuctions.py
@@ -0,0 +1,16 @@
+from functions import add,subtract, multiply
+from functions import convert_fahr_to_c as f2c
+
+def test_add():
+	assert add(2,3) ==5
+	assert add("space","ship")=="spaceship"
+
+
+#def test_subtract():
+#	assert subtract(2,3)==-1
+
+#def test_covert_fahr_to_c():
+#	assert f2c(32)==0
+#	assert f2c(122)== pytest.approx(50)
+# 	with pytest.raises (AssertionError)
+#		f2c(-600)
