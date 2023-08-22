68a887a820ce13b6dd58c537da299a3599df4b49
Luis <arenasvillamizar.luiscarlos@gmail.com>
2023-08-22 14:59:41 -0500

    NUEvo

--- test_fuctions.py
+++ test_fuctions.py
@@ -1,6 +1,6 @@
 from functions import add,subtract, multiply
 from functions import convert_fahr_to_c as f2c
-
+import pytest
 def test_add():
 	assert add(2,3) ==5
 	assert add("space","ship")=="spaceship"
